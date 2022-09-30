5aa08e95b31447d973806d657720be4519708b0f
Esau Martinez <esaumtz4722@gmail.com>
2022-09-30 11:34:46 -0500

    feat: add game data

---
+++ src/data/games.js
@@ -0,0 +1,11 @@
+
+const games = {
+    '1': {
+        id: 1,
+        name: 'Adivina el número',
+        description: 'Adivina el número que estoy pensando',
+        image: 'https://i.imgur.com/3J3YQYx.png',
+    },
+}
+
+export default games;
