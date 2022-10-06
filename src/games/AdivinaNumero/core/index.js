498acb0cf8c10cd9b8bd2356973dd85d99676ab1
Esau Martinez <esaumtz4722@gmail.com>
2022-10-06 13:32:26 -0500

    fix: update core check numbers

--- src/games/AdivinaNumero/core/index.js
+++ src/games/AdivinaNumero/core/index.js
@@ -13,32 +13,14 @@ export const randomChoice = (n = 1, min = 1, max = 10) => {
 	return result;
 };
 
-// function that receives two arrays of numbers 
-// checks if the numbers are the same number and in the same position
-// also check how many numbers are the same but in different position
-// also check if numbers in array 1 are bigger than numbers in array 2
-// returns an array with the number of numbers in the same position, the number of numbers in different position and if the first array is bigger than the second array
-export const checkNumbers = (numbers, randomNumbers) => {
-	let samePosition = 0;
-	let sameNumber = 0;
-	for (let i = 0; i < numbers.length; i++) {
-		if (numbers[i] === randomNumbers[i]) {
-			samePosition++;
-		} else if (randomNumbers.includes(numbers[i])) {
-			sameNumber++;
-		}
+// function that receives two numbers
+// returns if its bigger, smaller or equal
+// return values: 1, -1, 0
+export const checkNumbers = (a, b) => {
+	if (a > b) {
+		return 1;
+	} else if (a < b) {
+		return -1;
 	}
-
-	// check if numbers are bigger
-	let bigger = false;
-	for (let i = 0; i < numbers.length; i++) {
-		if (numbers[i] > randomNumbers[i]) {
-			bigger = true;
-			break;
-		} else if (numbers[i] < randomNumbers[i]) {
-			break;
-		}
-	}
-
-	return [samePosition, sameNumber, bigger];
+	return 0;
 };
