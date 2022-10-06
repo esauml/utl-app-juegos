0bab88fdc683a908d64749c3377842fbf3c93cfa
MikeRojas22 <jrojas6805@gmail.com>
2022-10-06 14:54:13 -0500

    Update GamePage.jsx

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,4 +1,5 @@
 import { Container, Paper } from "@mui/material";
+import { Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import React from "react";
 import { useParams } from "react-router-dom";
@@ -6,32 +7,60 @@ import { GameStrategy } from "@/utilities";
 import { getGameById } from "../../data/games";
 
 const GamePage = () => {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const { id: gameId, name, description } = getGameById(Number(id));
+  const { id: gameId, name, description } = getGameById(Number(id));
 
-    return (
-        <>
-            <h1 sx={{ p: 2, margin: 'auto', maxWidth: 500, flexGrow: 1 }}>Game Page</h1>
+  return (
+    <>
+      <Typography
+        variant="h2"
+        align="center"
+        sx={{ m: 2, color: "primary.main" }}
+      >
+        {name}
+      </Typography>
+      <Container maxWidth="xl" sx={{ justifyContent: "center" }}>
+        {/* Two aside sections  with Stack*/}
+        <Stack direction="row" spacing={2}>
+          <Paper
+            sx={{
+              p: 2,
+              textAlign: "center",
+              color: "text.secondary",
+              flex: "1 1 0",
+            }}
+          >
+            <GameStrategy id={id} />
+          </Paper>
 
-            <Container maxWidth="xl" sx={{ justifyContent: 'center' }}>
+          <Paper sx={{ p: 2, maxWidth: "700px" }}>
+            <Typography variant="h5" align="center">
+              Ficha tecnica del juego
+            </Typography>
+            <Typography
+              variant="h6"
+              align="left"
+              sx={{ color: "text.primary", mt: 2 }}
+            >
+              Nombre: {name}
+            </Typography>
 
-                {/* Two aside sections  with Stack*/}
-                <Stack direction="row" spacing={2}>
-                    <Paper sx={{ flexGrow: 1, }}>
-                        <GameStrategy id={id} />
-                    </Paper>
-
-                    <Paper sx={{ p: 2, maxWidth: 500, flexGrow: 1 }}>
-                        <h2>Game Info</h2>
-                        <p>Game ID: {gameId}</p>
-                        <p>Titulo : {name}</p>
-                        <p>Descripcion: {description}</p>
-                    </Paper>
-                </Stack>
-            </Container>
-        </>
-    )
-}
+            <Typography
+              variant="h6"
+              align="left"
+              sx={{ color: "text.secondary", mt: 2 }}
+            >
+              Descripción
+            </Typography>
+            <Typography variant="body1" align="left">
+              {description}
+            </Typography>
+          </Paper>
+        </Stack>
+      </Container>
+    </>
+  );
+};
 
 export default GamePage;
